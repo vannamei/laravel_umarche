138f291703652f2e8de19a05c25c715bb31e5423
vannamei <shun_ymnk@icloud.com>
2022-05-17 14:49:09 +0900

    :sparkles: add props

--- resources/views/tests/component-test1.blade.php
+++ resources/views/tests/component-test1.blade.php
@@ -2,6 +2,5 @@
   <x-slot name="header">ヘッダー1</x-slot>
 コンポーネントテスト1
   <x-tests.card title="タイトル1" content="本文" :message="$message" />
-  <x-tests.card title="タイトル2" content="本文" :message="$message" />
-  <x-tests.card title="タイトル3" content="本文" :message="$message" />
+  <x-tests.card title="タイトル2" />
 </x-tests.app>
