9c6234f81bd0e1f8d8702531695375f794dfa63f
vannamei <shun_ymnk@icloud.com>
2022-05-17 14:36:54 +0900

    :sparkles: add attributes

--- resources/views/tests/component-test1.blade.php
+++ resources/views/tests/component-test1.blade.php
@@ -1,4 +1,7 @@
 <x-tests.app>
   <x-slot name="header">ヘッダー1</x-slot>
 コンポーネントテスト1
+  <x-tests.card title="タイトル1" content="本文" />
+  <x-tests.card title="タイトル2" content="本文" />
+  <x-tests.card title="タイトル3" content="本文" />
 </x-tests.app>
