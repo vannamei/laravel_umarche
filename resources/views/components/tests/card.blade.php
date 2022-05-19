818fd2bd48662cfb153d84c9f1a8c84b675fcc18
vannamei <shun_ymnk@icloud.com>
2022-05-19 10:31:35 +0900

    :sparkles: add service provider

--- resources/views/components/tests/card.blade.php
+++ resources/views/components/tests/card.blade.php
@@ -5,7 +5,7 @@
 ])
 
 <div {{ $attributes->merge(['class' => 'border-2 shadow-md w-1/4 p-2']) }}>
-    <div>{{ $title }}</div>
+       <div>{{ $title }}</div>
     <div>画像</div>
     <div>{{ $content }}</div>
     <div>{{ $message }}</div>
