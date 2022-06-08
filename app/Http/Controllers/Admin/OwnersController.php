7414211691733a1869d8ea60660a6ff1794a38de
vannamei <shun_ymnk@icloud.com>
2022-06-08 11:53:32 +0900

    :sparkles: Add pagination

--- app/Http/Controllers/Admin/OwnersController.php
+++ app/Http/Controllers/Admin/OwnersController.php
@@ -17,7 +17,7 @@ class OwnersController extends Controller
 
     public function index()
     {
-        $owners = Owner::select('id', 'name', 'email', 'created_at')->get();
+        $owners = Owner::select('id', 'name', 'email', 'created_at')->paginate(5);
         return view('admin.owners.index')
             ->with('owners', $owners);
     }
